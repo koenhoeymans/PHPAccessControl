f0157039f5345db3850bf0bba2d5deb82f0ec637
koenhoeymans <koenhoeymans@gmail.com>
2019-09-17 14:32:56 +0200

    make PSR2 compliant

--- tests/e2e/PHPAccessControlTest.php
+++ tests/e2e/PHPAccessControlTest.php
@@ -4,368 +4,368 @@ namespace PHPAccessControl;
 
 class PHPAccessControlTest extends \PHPUnit\Framework\TestCase
 {
-	public function setup()
-	{
-		$this->accessControl = SetupCreator::create();
-	}
+    public function setup()
+    {
+        $this->accessControl = SetupCreator::create();
+    }
 
-	/**
-	 * @test
-	 */
-	public function accessIsDeniedByDefault()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function accessIsDeniedByDefault()
+    {
+        $ac = $this->accessControl;
 
-		$result = $ac->checkPermissionOf(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')
-		);
+        $result = $ac->checkPermissionOf(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')
+        );
 
-		$this->assertFalse($result->isAllowed());
-	}
+        $this->assertFalse($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function aSubjectCanBeAllowedAnActionOnAnObject()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function aSubjectCanBeAllowedAnActionOnAnObject()
+    {
+        $ac = $this->accessControl;
 
-		$ac->allow(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')
-		);
+        $ac->allow(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')
+        );
 
-		$result = $ac->checkPermissionOf(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')
-		);
+        $result = $ac->checkPermissionOf(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')
+        );
 
-		$this->assertTrue($result->isAllowed());
-	}
+        $this->assertTrue($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function aSubjectCanBeDeniedAnActionOnAnObject()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function aSubjectCanBeDeniedAnActionOnAnObject()
+    {
+        $ac = $this->accessControl;
 
-		$ac->deny(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')
-		);
+        $ac->deny(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')
+        );
 
-		$result = $ac->checkPermissionOf(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')
-		);
+        $result = $ac->checkPermissionOf(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')
+        );
 
-		$this->assertFalse($result->isAllowed());
-	}
+        $this->assertFalse($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function aSubjectCanBeAllowedAnActionOnAnObjectThatIsSpecified()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function aSubjectCanBeAllowedAnActionOnAnObjectThatIsSpecified()
+    {
+        $ac = $this->accessControl;
 
-		$ac->allow(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->equals(5))
-		);
+        $ac->allow(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->equals(5))
+        );
 
-		$result = $ac->checkPermissionOf(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->equals(5))
-		);
-		$this->assertTrue($result->isAllowed());
-	}
+        $result = $ac->checkPermissionOf(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->equals(5))
+        );
+        $this->assertTrue($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function moreSpecificObjectsInheritPermissionOfLessSpecificObjects()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function moreSpecificObjectsInheritPermissionOfLessSpecificObjects()
+    {
+        $ac = $this->accessControl;
 
-		$ac->allow(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->lesserThan(5))
-		);
+        $ac->allow(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->lesserThan(5))
+        );
 
-		$result = $ac->checkPermissionOf(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->equals(4))
-		);
+        $result = $ac->checkPermissionOf(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->equals(4))
+        );
 
-		$this->assertTrue($result->isAllowed());
-	}
+        $this->assertTrue($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function whenMoreThanOneRuleAppliesTheMostSpecificMatchingRuleWins()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function whenMoreThanOneRuleAppliesTheMostSpecificMatchingRuleWins()
+    {
+        $ac = $this->accessControl;
 
-		$ac->allow(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->lesserThan(5))
-		);
+        $ac->allow(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->lesserThan(5))
+        );
 
-		$ac->deny(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->lesserThan(3))
-		);
+        $ac->deny(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->lesserThan(3))
+        );
 
-		$result = $ac->checkPermissionOf(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->equals(2))
-		);
-		$this->assertFalse($result->isAllowed());
+        $result = $ac->checkPermissionOf(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->equals(2))
+        );
+        $this->assertFalse($result->isAllowed());
 
-		$result = $ac->checkPermissionOf(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->equals(4))
-		);
+        $result = $ac->checkPermissionOf(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->equals(4))
+        );
 
-		$this->assertTrue($result->isAllowed());
-	}
+        $this->assertTrue($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function allowWinsFromDenyIfBothAreEquallySpecific()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function allowWinsFromDenyIfBothAreEquallySpecific()
+    {
+        $ac = $this->accessControl;
 
-		$ac->allow(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->lesserThan(5))
-		);
+        $ac->allow(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->lesserThan(5))
+        );
 
-		$ac->deny(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->greaterThan(3))
-		);
+        $ac->deny(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->greaterThan(3))
+        );
 
-		$result = $ac->checkPermissionOf(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->equals(4))
-		);
+        $result = $ac->checkPermissionOf(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->equals(4))
+        );
 
-		$this->assertTrue($result->isAllowed());
-	}
+        $this->assertTrue($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function accessRightsCanBeConditional()
-	{
-		// given
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function accessRightsCanBeConditional()
+    {
+        // given
+        $ac = $this->accessControl;
 
-		$ac->allow(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->lesserThan(5))
-		);
+        $ac->allow(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->lesserThan(5))
+        );
 
-		// when
-		$result = $ac->checkPermissionOf(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')
-		);
+        // when
+        $result = $ac->checkPermissionOf(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')
+        );
 
-		// then
-		$this->assertTrue($result->isConditional());
-	}
+        // then
+        $this->assertTrue($result->isConditional());
+    }
 
-	/**
-	 * @test
-	 */
-	public function whenSituationIsAllowedAndMoreSpecificOneDeniedAccessIsConditional()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function whenSituationIsAllowedAndMoreSpecificOneDeniedAccessIsConditional()
+    {
+        $ac = $this->accessControl;
 
-		// given
-		$ac->allow(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')
-		);
+        // given
+        $ac->allow(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')
+        );
 
-		$ac->deny(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')->with($ac->property('postcount')->lesserThan(5))
-		);
+        $ac->deny(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')->with($ac->property('postcount')->lesserThan(5))
+        );
 
-		// when
-		$result = $ac->checkPermissionOf(
-			$ac->a('guest'),
-			$ac->to('view'),
-			$ac->a('post')
-		);
+        // when
+        $result = $ac->checkPermissionOf(
+            $ac->a('guest'),
+            $ac->to('view'),
+            $ac->a('post')
+        );
 
-		// then
-		$conditions = $result->getAllowed();
-		$this->assertEquals(
-			$conditions,
-			$ac->not($ac->a('post')->with($ac->property('postcount')->lesserThan(5)))
-		);
-	}
+        // then
+        $conditions = $result->getAllowed();
+        $this->assertEquals(
+            $conditions,
+            $ac->not($ac->a('post')->with($ac->property('postcount')->lesserThan(5)))
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function roleBasedUsageAllowsMultipleRolesInAccessQuery()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function roleBasedUsageAllowsMultipleRolesInAccessQuery()
+    {
+        $ac = $this->accessControl;
 
-		// given
-		$ac->allow(
-			$ac->a('admin'),
-			$ac->to('delete'),
-			$ac->a('post')
-		);
+        // given
+        $ac->allow(
+            $ac->a('admin'),
+            $ac->to('delete'),
+            $ac->a('post')
+        );
 
-		$ac->deny(
-			$ac->a('user'),
-			$ac->to('delete'),
-			$ac->a('post')
-		);
+        $ac->deny(
+            $ac->a('user'),
+            $ac->to('delete'),
+            $ac->a('post')
+        );
 
-		// when
-		$result = $ac->checkPermissionOf(
-			$ac->a('admin')->lAnd($ac->a('user')),
-			$ac->to('delete'),
-			$ac->a('post')
-		);
+        // when
+        $result = $ac->checkPermissionOf(
+            $ac->a('admin')->lAnd($ac->a('user')),
+            $ac->to('delete'),
+            $ac->a('post')
+        );
 
-		// then
-		$this->assertTrue($result->isAllowed());
-	}
+        // then
+        $this->assertTrue($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function everySubjectInheritsFromTheSpecialAnyAco()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function everySubjectInheritsFromTheSpecialAnyAco()
+    {
+        $ac = $this->accessControl;
 
-		// given
-		$ac->allow(
-			$ac->any(),
-			$ac->to('delete'),
-			$ac->a('post')
-		);
+        // given
+        $ac->allow(
+            $ac->any(),
+            $ac->to('delete'),
+            $ac->a('post')
+        );
 
-		// when
-		$result = $ac->checkPermissionOf(
-			$ac->a('admin'),
-			$ac->to('delete'),
-			$ac->a('post')
-		);
+        // when
+        $result = $ac->checkPermissionOf(
+            $ac->a('admin'),
+            $ac->to('delete'),
+            $ac->a('post')
+        );
 
-		// then
-		$this->assertTrue($result->isAllowed());
-	}
+        // then
+        $this->assertTrue($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function everyObjectInheritsFromTheSpecialAnyAco()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function everyObjectInheritsFromTheSpecialAnyAco()
+    {
+        $ac = $this->accessControl;
 
-		// given
-		$ac->allow(
-			$ac->a('admin'),
-			$ac->to('delete'),
-			$ac->any()
-		);
+        // given
+        $ac->allow(
+            $ac->a('admin'),
+            $ac->to('delete'),
+            $ac->any()
+        );
 
-		// when
-		$result = $ac->checkPermissionOf(
-			$ac->a('admin'),
-			$ac->to('delete'),
-			$ac->a('post')
-		);
+        // when
+        $result = $ac->checkPermissionOf(
+            $ac->a('admin'),
+            $ac->to('delete'),
+            $ac->a('post')
+        );
 
-		// then
-		$this->assertTrue($result->isAllowed());
-	}
+        // then
+        $this->assertTrue($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function everyActionInheritsFromTheSpecialAnyAction()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function everyActionInheritsFromTheSpecialAnyAction()
+    {
+        $ac = $this->accessControl;
 
-		// given
-		$ac->allow(
-			$ac->a('admin'),
-			$ac->anyAction(),
-			$ac->a('post')
-		);
+        // given
+        $ac->allow(
+            $ac->a('admin'),
+            $ac->anyAction(),
+            $ac->a('post')
+        );
 
-		// when
-		$result = $ac->checkPermissionOf(
-			$ac->a('admin'),
-			$ac->to('delete'),
-			$ac->a('post')
-		);
+        // when
+        $result = $ac->checkPermissionOf(
+            $ac->a('admin'),
+            $ac->to('delete'),
+            $ac->a('post')
+        );
 
-		// then
-		$this->assertTrue($result->isAllowed());
-	}
+        // then
+        $this->assertTrue($result->isAllowed());
+    }
 
-	/**
-	 * @test
-	 */
-	public function acoInheritanceCanBeManuallyAdded()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function acoInheritanceCanBeManuallyAdded()
+    {
+        $ac = $this->accessControl;
 
-		// given
-		$ac->allow(
-			$ac->a('admin'),
-			$ac->to('edit'),
-			$ac->a('catZ')
-		);
+        // given
+        $ac->allow(
+            $ac->a('admin'),
+            $ac->to('edit'),
+            $ac->a('catZ')
+        );
 
-		$ac->addParentAco($ac->a('catX'))->to($ac->a('catY'));
-		$ac->addParentAco($ac->a('catZ'))->to($ac->a('catX'));
+        $ac->addParentAco($ac->a('catX'))->to($ac->a('catY'));
+        $ac->addParentAco($ac->a('catZ'))->to($ac->a('catX'));
 
-		// when
-		$result = $ac->checkPermissionOf(
-			$ac->a('admin'),
-			$ac->to('edit'),
-			$ac->a('catY')
-		);
+        // when
+        $result = $ac->checkPermissionOf(
+            $ac->a('admin'),
+            $ac->to('edit'),
+            $ac->a('catY')
+        );
 
-		// then
-		$this->assertTrue($result->isAllowed());
-	}
+        // then
+        $this->assertTrue($result->isAllowed());
+    }
 }
--- tests/e2e/UsageAsRoleBasedAccessControlTest.php
+++ tests/e2e/UsageAsRoleBasedAccessControlTest.php
@@ -6,83 +6,83 @@ namespace PHPAccessControl;
  * Takes the example in the Zend Framework ACL documentation as a use case.
  * See http://framework.zend.com/manual/en/zend.acl.html (version 1.10)
  */
-class usageAsRoleBasedAccessControl extends \PHPUnit\Framework\TestCase
+class UsageAsRoleBasedAccessControl extends \PHPUnit\Framework\TestCase
 {
-	public function setup()
-	{
-		$this->accessControl = SetupCreator::create();
-	}
+    public function setup()
+    {
+        $this->accessControl = SetupCreator::create();
+    }
 
-	/**
-	 * @test
-	 */
-	public function usageAsRoleBaseAccessControl()
-	{
-		$ac = $this->accessControl;
+    /**
+     * @test
+     */
+    public function usageAsRoleBaseAccessControl()
+    {
+        $ac = $this->accessControl;
 
-		$ac->addParentAco($ac->a('guest'))->to($ac->a('staff'));
-		$ac->addParentAco($ac->a('staff'))->to($ac->a('editor'));
+        $ac->addParentAco($ac->a('guest'))->to($ac->a('staff'));
+        $ac->addParentAco($ac->a('staff'))->to($ac->a('editor'));
 
-		$ac->allow($ac->a('guest'), $ac->to('view'), $ac->any());
-		$ac->allow($ac->a('staff'), $ac->to('edit'), $ac->any());
-		$ac->allow($ac->a('staff'), $ac->to('submit'), $ac->any());
-		$ac->allow($ac->a('staff'), $ac->to('revise'), $ac->any());
-		$ac->allow($ac->a('editor'), $ac->to('publish'), $ac->any());
-		$ac->allow($ac->a('editor'), $ac->to('archive'), $ac->any());
-		$ac->allow($ac->a('editor'), $ac->to('delete'), $ac->any());
-		$ac->allow($ac->a('administrator'), $ac->anyAction(), $ac->any());
+        $ac->allow($ac->a('guest'), $ac->to('view'), $ac->any());
+        $ac->allow($ac->a('staff'), $ac->to('edit'), $ac->any());
+        $ac->allow($ac->a('staff'), $ac->to('submit'), $ac->any());
+        $ac->allow($ac->a('staff'), $ac->to('revise'), $ac->any());
+        $ac->allow($ac->a('editor'), $ac->to('publish'), $ac->any());
+        $ac->allow($ac->a('editor'), $ac->to('archive'), $ac->any());
+        $ac->allow($ac->a('editor'), $ac->to('delete'), $ac->any());
+        $ac->allow($ac->a('administrator'), $ac->anyAction(), $ac->any());
 
-		$result = $ac->checkPermissionOf($ac->a('guest'), $ac->to('view'), $ac->any());
-		$this->assertTrue($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('staff'), $ac->to('publish'), $ac->any());
-		$this->assertFalse($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('staff'), $ac->to('revise'), $ac->any());
-		$this->assertTrue($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('editor'), $ac->to('view'), $ac->any());
-		$this->assertTrue($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('editor'), $ac->to('update'), $ac->any());
-		$this->assertFalse($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('administrator'), $ac->to('view'), $ac->any());
-		$this->assertTrue($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('administrator'), $ac->anyAction(), $ac->any());
-		$this->assertTrue($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('administrator'), $ac->to('update'), $ac->any());
-		$this->assertTrue($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('guest'), $ac->to('view'), $ac->any());
+        $this->assertTrue($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('staff'), $ac->to('publish'), $ac->any());
+        $this->assertFalse($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('staff'), $ac->to('revise'), $ac->any());
+        $this->assertTrue($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('editor'), $ac->to('view'), $ac->any());
+        $this->assertTrue($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('editor'), $ac->to('update'), $ac->any());
+        $this->assertFalse($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('administrator'), $ac->to('view'), $ac->any());
+        $this->assertTrue($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('administrator'), $ac->anyAction(), $ac->any());
+        $this->assertTrue($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('administrator'), $ac->to('update'), $ac->any());
+        $this->assertTrue($result->isAllowed());
 
-		$ac->addParentAco($ac->a('staff'))->to($ac->a('marketing'));
+        $ac->addParentAco($ac->a('staff'))->to($ac->a('marketing'));
 
-		$ac->allow($ac->a('marketing'), $ac->to('publish'), $ac->a('newsletter'));
-		$ac->allow($ac->a('marketing'), $ac->to('archive'), $ac->a('newsletter'));
-		$ac->allow($ac->a('marketing'), $ac->to('publish'), $ac->a('latest'));
-		$ac->allow($ac->a('marketing'), $ac->to('archive'), $ac->a('latest'));
-		$ac->deny($ac->a('staff'), $ac->to('revise'), $ac->a('latest'));
-		$ac->deny($ac->any(), $ac->to('archive'), $ac->a('announcement'));
+        $ac->allow($ac->a('marketing'), $ac->to('publish'), $ac->a('newsletter'));
+        $ac->allow($ac->a('marketing'), $ac->to('archive'), $ac->a('newsletter'));
+        $ac->allow($ac->a('marketing'), $ac->to('publish'), $ac->a('latest'));
+        $ac->allow($ac->a('marketing'), $ac->to('archive'), $ac->a('latest'));
+        $ac->deny($ac->a('staff'), $ac->to('revise'), $ac->a('latest'));
+        $ac->deny($ac->any(), $ac->to('archive'), $ac->a('announcement'));
 
-		$result = $ac->checkPermissionOf($ac->a('staff'), $ac->to('publish'), $ac->a('newsletter'));
-		$this->assertFalse($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('marketing'), $ac->to('publish'), $ac->a('newsletter'));
-		$this->assertTrue($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('staff'), $ac->to('publish'), $ac->a('latest'));
-		$this->assertFalse($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('marketing'), $ac->to('publish'), $ac->a('latest'));
-		$this->assertTrue($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('marketing'), $ac->to('archive'), $ac->a('latest'));
-		$this->assertTrue($result->isAllowed());
-		$result = $ac->checkPermissionOf($ac->a('marketing'), $ac->to('latest'), $ac->a('revise'));
-		$this->assertFalse($result->isAllowed());
-		/**
-		 * note: acces for editor-archive-announcement = contrast with Zend ACL
-		 * + allowed editor to archive anything
-		 * - denied any to archive announcement
-		 * = competing rules here on same level => allow wins from deny
-		 */
-		$result = $ac->checkPermissionOf($ac->a('editor'), $ac->to('archive'), $ac->a('announcement'));
-		$this->assertTrue($result->isAllowed());
-		/**
-		 * Same difference here.
-		 * There's a different lineage that allows this. So allow wins from deny.
-		 */
-		$result = $ac->checkPermissionOf($ac->a('administrator'), $ac->to('announcement'), $ac->a('archive'));
-		$this->assertTrue($result->isAllowed());
-	}
+        $result = $ac->checkPermissionOf($ac->a('staff'), $ac->to('publish'), $ac->a('newsletter'));
+        $this->assertFalse($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('marketing'), $ac->to('publish'), $ac->a('newsletter'));
+        $this->assertTrue($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('staff'), $ac->to('publish'), $ac->a('latest'));
+        $this->assertFalse($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('marketing'), $ac->to('publish'), $ac->a('latest'));
+        $this->assertTrue($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('marketing'), $ac->to('archive'), $ac->a('latest'));
+        $this->assertTrue($result->isAllowed());
+        $result = $ac->checkPermissionOf($ac->a('marketing'), $ac->to('latest'), $ac->a('revise'));
+        $this->assertFalse($result->isAllowed());
+        /**
+         * note: acces for editor-archive-announcement = contrast with Zend ACL
+         * + allowed editor to archive anything
+         * - denied any to archive announcement
+         * = competing rules here on same level => allow wins from deny
+         */
+        $result = $ac->checkPermissionOf($ac->a('editor'), $ac->to('archive'), $ac->a('announcement'));
+        $this->assertTrue($result->isAllowed());
+        /**
+         * Same difference here.
+         * There's a different lineage that allows this. So allow wins from deny.
+         */
+        $result = $ac->checkPermissionOf($ac->a('administrator'), $ac->to('announcement'), $ac->a('archive'));
+        $this->assertTrue($result->isAllowed());
+    }
 }
