25c1667e579fd703b527055a8492fd41f5ff62be
koenhoeymans <koenhoeymans@gmail.com>
2019-09-17 14:05:24 +0200

    make PSR2 compliant

--- src/Action/Action.php
+++ src/Action/Action.php
@@ -4,31 +4,30 @@ namespace PHPAccessControl\Action;
 
 class Action extends \PHPAccessControl\Specification\LeafSpecification
 {
-	const ANY_ACTION = 'any action';
+    const ANY_ACTION = 'any action';
 
-	private $action;
+    private $action;
 
-	public function __construct($action = self::ANY_ACTION)
-	{
-		$this->action = (string) $action;
-	}
+    public function __construct($action = self::ANY_ACTION)
+    {
+        $this->action = (string) $action;
+    }
 
-	public function isSatisfiedBy($candidate)
-	{
-		return $candidate === $this->action;
-	}
+    public function isSatisfiedBy($candidate)
+    {
+        return $candidate === $this->action;
+    }
 
-	public function isSpecialCaseOfAction(Action $action)
-	{
-		if ($action->action === self::ANY_ACTION)
-		{
-			return true;
-		}
-		return $action->action === $this->action;
-	}
+    public function isSpecialCaseOfAction(Action $action)
+    {
+        if ($action->action === self::ANY_ACTION) {
+            return true;
+        }
+        return $action->action === $this->action;
+    }
 
-	public function isGeneralizationOfAction(Action $action)
-	{
-		return $action->isSpecialCaseOf($this);
-	}
+    public function isGeneralizationOfAction(Action $action)
+    {
+        return $action->isSpecialCaseOf($this);
+    }
 }
