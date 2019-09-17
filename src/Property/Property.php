18a844a00093a3e03b2d7ecf43396558ac056c09
koenhoeymans <koenhoeymans@gmail.com>
2019-09-17 14:09:02 +0200

    make PSR2 compliant

--- src/Property/Property.php
+++ src/Property/Property.php
@@ -4,47 +4,44 @@ namespace PHPAccessControl\Property;
 
 class Property extends \PHPAccessControl\Specification\LeafSpecification
 {
-	protected $name;
+    protected $name;
 
-	protected $specification;
+    protected $specification;
 
-	public function __construct(
-		$name,
-		\PHPAccessControl\Specification\Specification $specification = null
-	) {
-		$this->name = $name;
-		$this->specification = $specification;
-	}
+    public function __construct(
+        $name,
+        \PHPAccessControl\Specification\Specification $specification = null
+    ) {
+        $this->name = $name;
+        $this->specification = $specification;
+    }
 
-	public static function named($name)
-	{
-		$class = get_called_class();
-		return new $class($name);
-	}
+    public static function named($name)
+    {
+        $class = get_called_class();
+        return new $class($name);
+    }
 
-	private function createPropertyWith(\PHPAccessControl\Specification\Specification $specification)
-	{
-		if ($this->specification !== null)
-		{
-			$specification = $this->specification->lAnd();
-		}
-		return new self($this->name, $specification);
-	}
+    private function createPropertyWith(\PHPAccessControl\Specification\Specification $specification)
+    {
+        if ($this->specification !== null) {
+            $specification = $this->specification->lAnd();
+        }
+        return new self($this->name, $specification);
+    }
 
-	protected function isSpecialCaseOfProperty(Property $property)
-	{
-		if ($this->specification)
-		{
-			if(!$this->specification->isSpecialCaseOf($property->specification))
-			{
-				return false;
-			}
-		}
-		return $this->name === $property->name;
-	}
+    protected function isSpecialCaseOfProperty(Property $property)
+    {
+        if ($this->specification) {
+            if (!$this->specification->isSpecialCaseOf($property->specification)) {
+                return false;
+            }
+        }
+        return $this->name === $property->name;
+    }
 
-	protected function isGeneralizationOfProperty(Property $property)
-	{
-		return $property->isSpecialCaseOf($this);
-	}
+    protected function isGeneralizationOfProperty(Property $property)
+    {
+        return $property->isSpecialCaseOf($this);
+    }
 }
--- src/Property/PropertyDSL.php
+++ src/Property/PropertyDSL.php
@@ -4,33 +4,32 @@ namespace PHPAccessControl\Property;
 
 class PropertyDSL extends Property
 {
-	public function equals($int)
-	{
-		$specification =
-			new \PHPAccessControl\Specification\ValueBoundSpecification\Equals($int);
-		return $this->createPropertyWith($specification);
-	}
+    public function equals($int)
+    {
+        $specification =
+            new \PHPAccessControl\Specification\ValueBoundSpecification\Equals($int);
+        return $this->createPropertyWith($specification);
+    }
 
-	public function lesserThan($int)
-	{
-		$specification =
-			new \PHPAccessControl\Specification\ValueBoundSpecification\LesserThan($int);
-		return $this->createPropertyWith($specification);
-	}
+    public function lesserThan($int)
+    {
+        $specification =
+            new \PHPAccessControl\Specification\ValueBoundSpecification\LesserThan($int);
+        return $this->createPropertyWith($specification);
+    }
 
-	public function greaterThan($int)
-	{
-		$specification =
-			new \PHPAccessControl\Specification\ValueBoundSpecification\GreaterThan($int);
-		return $this->createPropertyWith($specification);
-	}
+    public function greaterThan($int)
+    {
+        $specification =
+            new \PHPAccessControl\Specification\ValueBoundSpecification\GreaterThan($int);
+        return $this->createPropertyWith($specification);
+    }
 
-	protected function createPropertyWith(\PHPAccessControl\Specification\Specification $specification)
-	{
-		if ($this->specification !== null)
-		{
-			$specification = $this->specification->lAnd();
-		}
-		return new self($this->name, $specification);
-	}
+    protected function createPropertyWith(\PHPAccessControl\Specification\Specification $specification)
+    {
+        if ($this->specification !== null) {
+            $specification = $this->specification->lAnd();
+        }
+        return new self($this->name, $specification);
+    }
 }
